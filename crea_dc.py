32bc299c41f3fdd008fd5961310d06112eb5b777
KamiJuanmi <50448229+KamiJuanmi@users.noreply.github.com>
2022-06-02 20:30:45 +0200

    Changed to the final form

--- crea_dc.py
+++ crea_dc.py
@@ -1,4 +1,5 @@
 import sys
+import random
 
 print("Creando archivo docker-compose")
 
@@ -9,10 +10,16 @@ f = open("docker-compose-mult-ccd.yml", "w")
 f.write("version: \"3.9\"\nservices:\n")
 
 for i in range(0, n_ccd):
-    f.write(f"  ccd_simulator_{i}:\n")
+    aux = random.randint(0, 1)
+    # aux = True
+    if(aux):
+        simulador = "ccd_simulator"
+    else:
+        simulador = "mount_simulator"
+    f.write(f"  {simulador}_{i}:\n")
     f.write( "    image: juanmimm/ubuntu-indigo\n")
     f.write( "    ports:\n")
     f.write(f"      - 800{i}:800{i}\n")
-    f.write(f"    command : indigo_server -p 800{i} indigo_ccd_simulator\n")
+    f.write(f"    command : indigo_server -p 800{i} indigo_{simulador}\n")
 
 f.close()
--- lanza_docker.py
+++ lanza_docker.py
@@ -1,17 +1,17 @@
 import sys
 import subprocess
 
-if(len(sys.argv)<2):
-    n_ccd = "1"
-else:
-    n_ccd = sys.argv[1]
+# if(len(sys.argv)<2):
+#     n_ccd = "1"
+# else:
+#     n_ccd = sys.argv[1]
 
-with open("docker-compose-mult-ccd.yml", "r") as f:
-    n_lineas = len(f.readlines())
-    n_camaras = int((n_lineas-2)/5)
+# with open("docker-compose-mult-ccd.yml", "r") as f:
+#     n_lineas = len(f.readlines())
+#     n_camaras = int((n_lineas-2)/5)
 
-if(n_camaras != int(n_ccd)):
-    subprocess.call(["python", "crea_dc.py", n_ccd])
+# if(n_camaras != int(n_ccd)):
+#     subprocess.call(["python", "crea_dc.py", n_ccd])
 
 try:
     subprocess.call(["docker-compose", "-f", "docker-compose-mult-ccd.yml", "up", "--remove-orphans"])
